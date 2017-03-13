e5b39302ddfa26658449f6c6c40f94242ef06958
militoj <militoj@gmail.com>
2017-03-13 17:49:58 -0500

    added getters and setters to class

--- Student.java
+++ Student.java
@@ -9,5 +9,37 @@ public class Student {
     private int studentId;
     private int numberOfCredits;
     private double gpa;
-    
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double aGpa) {
+        gpa = aGpa;
+    }
 }
