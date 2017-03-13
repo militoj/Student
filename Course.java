3ead144c548b2f503ce2fdbb2fe20c33b0c7a70b
militoj <militoj@gmail.com>
2017-03-13 18:11:26 -0500

    intiated new class and declared variables

---
+++ Course.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.demos.java4python.school;
+
+import java.util.HashMap;
+
+/**
+ * Created by johnmilito on 3/13/17.
+ */
+public class Course {
+
+    private String courseName;
+    private String location;
+    private int courseNumber;
+    private HashMap<Integer, Integer> students = new HashMap<>();
+
+
+
+}
