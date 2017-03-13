214993f3ddce00ff30f387abbe06e3fdea5d0dce
militoj <militoj@gmail.com>
2017-03-13 17:37:17 -0500

    initialized student variables

--- Student.java
+++ Student.java
@@ -4,4 +4,10 @@ package org.launchcode.java.demos.java4python.school;
  * Created by johnmilito on 3/13/17.
  */
 public class Student {
+
+    private String name;
+    private int studentId;
+    private int numberOfCredits;
+    private double gpa;
+    
 }
